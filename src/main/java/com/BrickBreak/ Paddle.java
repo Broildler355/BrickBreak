396c4bd08fd9e37e184d402e83d694f2453e2908
Broildler355 <fouadk2024@gmail.com>
2025-12-16 16:03:58 +0000

    update 67

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -4,18 +4,64 @@ import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
-
-
+	private int height;
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int xVelo = 2;
+	private int yVelo = 0;
 	//your code here!
-
+	public Paddle(int x, int y, int w, int h) {
+		this.xPos = x;
+		this.yPos = y;
+		this.width = w;
+		this.height = h;
+	}
 
 	
 	//don't forget you need instance variables:
-	
+	public int getX() {
+		return xPos;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	public int getVelocity() {
+		return xVelo;
+	}
+	public void addVelocity(int z) {
+		xVelo += z;
+	}
+
+	public void setX(int x) {
+		this.x = xPos;
+	}
+
+	public int getY() {
+		return yPos;
+	}
+
+	public void setY(int y) {
+		this.y = yPos;
+	}
+
+	public void move() {
+		xPos += xVelo;
+	}
 	
 	//constructor(s):
 	
 	//methods:
-	
+	//precondition: g is not null
+	//postcondition: the brick is drawn using the color corresponding to its HP value.
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+           g.fillRect(x, y, width, height);
+	}
 	
 }
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -2,14 +2,83 @@ package com.BrickBreak;
 import java.awt.*;
 
 public class Ball {
+	private int radius;
+	private int xPos;
+	private int yPos;
+	private int xVelo = 3;
+	private int yVelo = 2;
 	//your code here!
+	public Ball(int x, int y, int r) {
+		this.xPos = x;
+		this.yPos = y;
+		this.radius = r;
+	}
+
 	
 	//don't forget you need instance variables:
-	
-	
+	public int getXPos() {
+		return xPos;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	public int getVelocity() {
+		return xVelo;
+	}
+	public void addVelocity(int z) {
+		xVelo += z;
+	}
+
+	public void setX(int z) {
+		this.xPos = z;
+	}
+
+	public void setXVelocity(int z) {
+		this.xVelo = z;
+	}
+
+	public void setYVelocity(int z) {
+		this.yVelo = z;
+	}
+
+	public void reverseX() {
+		this.xVelo *= -1;
+	}
+
+	public void reverseY() {
+		this.yVelo *= -1;
+	}
+
+	public int getYPos() {
+		return yPos;
+	}
+
+	public void setY(int z) {
+		this.yPos = z;
+	}
+
+	public int getSize() {
+		return radius;
+	}
+
+	public void move() {
+		xPos += xVelo;
+		yPos += yVelo;
+	}
 	
 	//constructor(s):
 	
 	//methods:
-
+	//precondition: g is not null
+	//postcondition: the brick is drawn using the color corresponding to its HP value.
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+           g.fillOval(x, y, radius, radius);
+	}
+	
 }
