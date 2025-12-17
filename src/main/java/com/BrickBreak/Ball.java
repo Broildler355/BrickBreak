b3302460208855dfda0cb426e221a71d39199f41
Broildler355 <fouadk2024@gmail.com>
2025-12-17 02:11:49 +0000

    update kira

--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -2,6 +2,9 @@ package com.BrickBreak;
 import java.awt.*;
 
 public class Ball {
+	// Fouad Kadry
+	//12/16/25
+	// This creates a ball that can be bounced off of objects as well as having its size changed
 	private int radius;
 	private int xPos;
 	private int yPos;
@@ -16,48 +19,63 @@ public class Ball {
 
 	
 	//don't forget you need instance variables:
+	//Precon: none
+	// Post: returns x position
 	public int getXpos() {
 		return xPos;
 	}
+	//Precon: none
+	// Post: returns x velocity
 	public int getVelocity() {
 		return xVelo;
 	}
+	// Pre: int input
+	// Post: changes velocity by input
 	public void addVelocity(int z) {
 		xVelo += z;
 	}
-
+	// Pre: int input
+	// Post: gets x position
 	public void setX(int z) {
 		this.xPos = z;
 	}
-
+	// Pre: int input
+	// Post: sets x velocity
 	public void setXVelocity(int z) {
 		this.xVelo = z;
 	}
-
+	// pre: int input
+	// Post: sets y velocity
 	public void setYVelocity(int z) {
 		this.yVelo = z;
 	}
-
+	//Precon: none
+	// Post: reverses x position
 	public void reverseX() {
 		this.xVelo *= -1;
 	}
-
+	//Precon: none
+	// Post: reverses x position
 	public void reverseY() {
 		this.yVelo *= -1;
 	}
-
+	//Precon: none
+	// Post: returns y position
 	public int getYpos() {
 		return yPos;
 	}
-
+	// Pre: int input
+	// Post: sets y position
 	public void setY(int z) {
 		this.yPos = z;
 	}
-
+	//Precon: none
+	// Post: returns radius
 	public int getSize() {
 		return radius;
 	}
-
+	//Precon: none
+	// Post: moves based on velocities
 	public void move() {
 		xPos += xVelo;
 		yPos += yVelo;
@@ -67,7 +85,7 @@ public class Ball {
 	
 	//methods:
 	//precondition: g is not null
-	//postcondition: the brick is drawn using the color corresponding to its HP value.
+	//postcondition: the ball is drawn
 	public void draw(Graphics g) {
 		g.setColor(Color.white);
            g.fillOval(xPos, yPos, radius, radius);
--- src/main/java/com/BrickBreak/Paddle.java
+++ src/main/java/com/BrickBreak/Paddle.java
@@ -1,9 +1,11 @@
 package com.BrickBreak;
 
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
+	// Fouad Kadry
+	//12/16/25
+	// This is a class that creates a paddle that you can move left and right, and can be shaped into any rectangular form
 	private int height;
 	private int xPos;
 	private int yPos;
@@ -19,40 +21,53 @@ public class Paddle {
 
 	
 	//don't forget you need instance variables:
+	//Precon: none
+	// Post: returns x position
 	public int getX() {
 		return xPos;
 	}
-
+	//Precon: none
+	// Post: returns ww\idth
 	public int getWidth() {
 		return width;
 	}
-
+	//Precon: none
+	// Post: returns height
 	public int getHeight() {
 		return height;
 	}
+	//Precon: none
+	// Post: returns velocity
 	public int getVelocity() {
 		return xVelo;
 	}
+	// Pre: int input
+	// Post: changes velocity by input
 	public void addVelocity(int z) {
 		xVelo += z;
 	}
-
+	// Pre: int input
+	// Post: sets new x position
 	public void setX(int x) {
 		this.xPos = x;
 	}
-
+	// Pre: int input
+	// Post: sets new velocity
 	public void setVelocity(int x) {
 		this.xVelo = x;
 	}
-
+	//Precon: none
+	// Post: returns y position
 	public int getY() {
 		return yPos;
 	}
-
+	// Pre: int input
+	// Post: sets new y position
 	public void setY(int y) {
 		this.yPos = y;
 	}
-
+	//Precon: none
+	// Post: moves on x-axis by x velocity
 	public void move() {
 		xPos += xVelo;
 	}
@@ -61,7 +76,7 @@ public class Paddle {
 	
 	//methods:
 	//precondition: g is not null
-	//postcondition: the brick is drawn using the color corresponding to its HP value.
+	//postcondition: the brick is drawn
 	public void draw(Graphics g) {
 		g.setColor(Color.white);
            g.fillRect(xPos, yPos, width, height);
